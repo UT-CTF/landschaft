ea6a952195d911ef68f28746de1708f0d1661090
pmarathi <pmarathi314@gmail.com>
2025-03-06 20:55:26 -0600

    added OS version

--- triage/main.go
+++ triage/main.go
@@ -1,12 +1,7 @@
 package triage
 
-import (
-	"fmt"
-)
-
 func Run() {
-	fmt.Println("Running triage")
-	printNetworkInfo()
 	printTriageMessage()
-	// print_software_versions()
+	printOSVersion()
+	printNetworkInfo()
 }
