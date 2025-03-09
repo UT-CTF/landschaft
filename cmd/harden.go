c2b4cebbc8324de811821844788843ce70b1c800
Brendan Early <mymindstorm@evermiss.net>
2025-03-09 16:58:20 -0500

    Apply suggestions from code review

--- cmd/harden.go
+++ cmd/harden.go
@@ -9,12 +9,6 @@ import (
 var hardenCmd = &cobra.Command{
 	Use:   "harden",
 	Short: "Various hardening scripts that need to be run individually",
-	// 	Long: `A longer description that spans multiple lines and likely contains examples
-	// and usage of using your command. For example:
-
-	// Cobra is a CLI library for Go that empowers applications.
-	// This application is a tool to generate the needed files
-	// to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		harden.Run(cmd)
 	},
@@ -24,14 +18,4 @@ func init() {
 	harden.SetupCommand(hardenCmd)
 
 	rootCmd.AddCommand(hardenCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// hardenCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// hardenCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
