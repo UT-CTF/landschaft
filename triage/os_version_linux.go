b152b2f08fbe34c9c17eed887594dd961f786bc1
pmarathi <pmarathi314@gmail.com>
2025-03-09 16:32:45 -0500

    added os version for linux

--- triage/os_version_linux.go
+++ triage/os_version_linux.go
@@ -1,5 +1,27 @@
 package triage
 
-func runOSVersionTriage() {
-	// placeholder
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func printOSVersion() {
+	file, err := os.Open("/etc/os-release")
+	if err != nil {
+		fmt.Println("Error reading OS release:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "PRETTY_NAME=") {
+			fmt.Println("OS:", strings.Trim(line[12:], `"`))
+		} else if strings.HasPrefix(line, "VERSION=") {
+			fmt.Println("Version:", strings.Trim(line[9:], `"`))
+		}
+	}
 }
