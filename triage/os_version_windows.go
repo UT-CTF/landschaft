1ea4c43d803495a750a69252a2cf729c720b952f
Brendan Early <mymindstorm@evermiss.net>
2025-03-08 21:40:54 -0600

    Refactor things to use RunAndPrintScript

--- triage/os_version_windows.go
+++ triage/os_version_windows.go
@@ -1,17 +1,9 @@
 package triage
 
 import (
-	"fmt"
-
-	"github.com/UT-CTF/landschaft/embed"
+	"github.com/UT-CTF/landschaft/util"
 )
 
 func runOSVersionTriage() {
-	fmt.Println("Executing triage/os_version.ps1")
-	scriptOut, err := embed.ExecuteScript("triage/os_version.ps1")
-	if err != nil {
-		fmt.Println("Error executing script: ", err)
-		return
-	}
-	fmt.Println(scriptOut)
+	util.RunAndPrintScript("triage/os_version.ps1")
 }
