61504529fefcf971a6c70a3eafcac6032652b044
pmarathi <pmarathi314@gmail.com>
2025-03-13 20:50:16 -0500

    pain

--- harden/add_local_admin_linux.go
+++ harden/add_local_admin_linux.go
@@ -8,19 +8,19 @@ import (
 
 func addLocalAdmin(username string) {
 	cmd := exec.Command("useradd", username, "--no-create-home", "--home-dir", "/")
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		fmt.Println("Error w/ useradd:", err)
 		return
 	}
-	cmd = exec.Command("usermod", "-aG", username)
+	cmd = exec.Command("usermod", "-aG", "0", username)
 	if err := cmd.Run(); err != nil {
 		fmt.Println("Error w/ usermod:", err)
 		return
 	}
 	cmd = exec.Command("passwd", username)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		fmt.Println("Error w/ passwd:", err)
 	}
