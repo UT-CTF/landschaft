b39efb77bbcbd953dcba335a5a7cb5c2769ab798
Ameya Purao <ameyapurao1@gmail.com>
2025-04-10 17:24:44 -0500

    firewall fix base

--- embed/windows/harden/firewall.ps1
+++ embed/windows/harden/firewall.ps1
@@ -1,44 +1,115 @@
 param (
     [switch]$Apply,
+    [switch]$Prune,
     [string]$RulePath,
-    [string]$BackupPath
+    [string]$BackupPath,
+    [string]$OldRulesPath,
+    [string]$Direction
 )
 
-if (-not $Apply) {
-    Get-Content firewall_rules.json | Write-Host
+if ($Prune) {
+    Write-Host "Removing all old inbound rules ..."
+    Get-Content $OldRulesPath | % {
+        if ($_.Trim().Length -eq 0) {
+            return
+        }
+        Write-Host "Removing rule with ID: $_"
+        try {
+            Remove-NetFirewallRule $_
+        }
+        catch {
+            Write-Host "Error removing rule: $($_.Exception.Message)"
+        }
+    }
     exit 0
 }
 
-Write-Host "Backing up current rules to $BackupPath ..."
-netsh advfirewall export $BackupPath
-
-$OldRules = Get-NetFirewallRule | ? { $_.Direction -eq "Inbound" }
-
-$rules = Get-Content $RulePath | ConvertFrom-Json
-
-$rules | % {
-    Write-Host "Creating rule for $($_.Name) ..."
-    $params = @{
-        DisplayName = $_.Name
-        Name = $_.Name
-        Direction = $_.Direction
-        Action = $_.Action
-        Protocol = $_.Protocol
-        LocalPort = $_.LocalPort
-        Profile = "Any"
-        Enabled = "True"
-    }
-
-    if ($_.Program) {
-        $params.Program = $_.Program
-    }
-
-    # Write-Host "New-NetFirewallRule $params"
-    # foreach ($key in $params.Keys) {
-    #     Write-Host "$key : $($params[$key])"
-    # }
-    New-NetFirewallRule @params
+if (-not $Apply) {
+    Get-Content $RulePath | Write-Host
+    exit 0
 }
 
-Write-Host "Removing existing inbound rules ..."
-$OldRules | Remove-NetFirewallRule
+try {
+    Write-Host "Backing up current rules to $BackupPath ..."
+    netsh advfirewall export $BackupPath
+}
+catch {
+    Write-Host "Error backing up rules: $($_.Exception.Message)"
+    exit 1
+}
+
+try {
+    $OldRules = Get-NetFirewallRule | ? { $_.Direction -eq $Direction }
+}
+catch {
+    Write-Host "Error getting current rules: $($_.Exception.Message)"
+    exit 1
+}
+
+try {
+(Get-Content $RulePath | ConvertFrom-Json -ErrorAction Stop) | % {
+        $params = @{}
+        $_.PSObject.Properties | % {
+            $params[$_.Name] = $_.Value
+        }
+        $params.Enabled = "True"
+        $params.Profile = "Any"
+        $params.ErrorAction = "Stop"
+        Write-Host "Creating rule: $($params.DisplayName)"
+        New-NetFirewallRule @params
+    }
+}
+catch {
+    Write-Host "Error creating rules: $($_.Exception.Message)"
+    exit 1
+}
+
+
+# $params = @{
+#     Test='hello'
+#     Second='bye'
+# }
+# $rules[0].PSObject.Properties | % { Write-Host $_.Value}
+# Write-Host $rules[0].GetType()
+# Write-Host $params.GetType()
+
+# try {
+#     $rules | % {
+#         Write-Host "Creating rule for $($_.Name) ..."
+#         $_ | % {
+#             Write-Host $_
+#         }
+#         $params = @{
+#             DisplayName = $_.Name
+#             Direction   = $_.Direction
+#             Action      = $_.Action
+#             Protocol    = $_.Protocol
+#             LocalPort   = $_.LocalPort
+#             Profile     = "Any"
+#             Enabled     = "True"
+#             ErrorAction = "Stop"
+#         }
+
+#         if ($_.Program) {
+#             $params.Program = $_.Program
+#         }
+
+#         # Write-Host "New-NetFirewallRule $params"
+#         # foreach ($key in $params.Keys) {
+#         #     Write-Host "$key : $($params[$key])"
+#         # }
+#         New-NetFirewallRule @params
+#     }
+# }
+# catch {
+#     Write-Host "Error creating rule: $($_.Exception.Message)"
+#     exit 1
+# }
+
+Write-Host "Firewall rules applied successfully."
+
+Write-Host "Writing old rules IDs to $OldRulesPath"
+$OldRules | Select-Object InstanceID | % {$_.InstanceID} | Out-File -FilePath $OldRulesPath
+
+# Write-Host "Removing existing inbound rules ..."
+# $OldRules | Remove-NetFirewallRule
--- harden/firewall_rules_linux.go
+++ harden/firewall_rules_linux.go
@@ -1,5 +1,17 @@
 package harden
 
-func configureFirewall() {
+// func configureFirewall(outbound bool, apply bool, ruleFile string) {
+// 	// placeholder
+// }
+
+func generateFirewallRules(outbound bool) {
+	// placeholder
+}
+
+func applyFirewallRules(rulesFile string, backupPath string, oldRulesFile string, direction string) {
+	// placeholder
+}
+
+func removeOldFirewallRules(oldRulesFile string) {
 	// placeholder
 }
