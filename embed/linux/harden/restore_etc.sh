ce9e5db75d3218e0583201f4dee1cabcfa630b96
pmarathi <pmarathi314@gmail.com>
2025-04-11 01:04:09 -0500

    added no_overwrite to avoid critical files

--- embed/linux/harden/restore_etc.sh
+++ embed/linux/harden/restore_etc.sh
@@ -1,7 +1,7 @@
     #!/bin/bash
     # Check for a given path
     if [ -z "$1" ]; then
-        echo "Please provide the path to the backup file."
+        echo "Backup file not found!"
         exit 1
     fi
     read -p "Are you sure you want to restore /etc from $1? (y/n): " response < /dev/tty
@@ -10,19 +10,13 @@
         exit 0
     fi
 
-    # Verify backup file exists
-    if [ ! -f "$1" ]; then
-        echo "Backup file not found!"
-        exit 1
-    fi
-
     # Check if file is a gzipped tar and contains /etc
     if ! tar -tzf "$1" | grep -q "^etc/"; then
         echo "Invalid backup file format!"
         exit 1
     fi
     # Restore the /etc/ folder from the backup
-    tar -xzf "$1" -C / --overwrite
+    tar -xzf "$1" -C / --overwrite --exclude-from="no_overwrite.txt"
 
     # status code
     if [ $? -eq 0 ]; then
